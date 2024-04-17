fc248527a0de2b0b7efbe628c9d830d7eb90830e
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-04-17 09:58:51 +0200

    Añadiendo los mensajes de error

--- spring-boot-form/src/main/java/com/nestor/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/nestor/springboot/form/app/controllers/FormController.java
@@ -18,7 +18,9 @@ public class FormController {
 
 	@GetMapping("/form")
 	public String form(Model model) {
+		Usuario usuario = new Usuario();
 		model.addAttribute("titulo", "Formulario usuarios");
+		model.addAttribute("usuario", usuario);
 		return "form";
 	}
 	
@@ -32,7 +34,7 @@ public class FormController {
 		if(result.hasErrors()) {
 			Map<String, String> errores = new HashMap<>();
 			result.getFieldErrors().forEach(err -> {
-				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+				errores.put(err.getField(), "El campo ".concat(err.getField()).concat("").concat(err.getDefaultMessage()));
 			});
 			model.addAttribute("error", errores);
 			return "form";
