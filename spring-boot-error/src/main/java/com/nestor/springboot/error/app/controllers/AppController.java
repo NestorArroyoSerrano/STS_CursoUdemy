a5e2e0739eaef2b537b5e215744b61e35724cde0
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-04-26 11:10:16 +0200

    ErrorHandlerController

---
+++ spring-boot-error/src/main/java/com/nestor/springboot/error/app/controllers/AppController.java
@@ -0,0 +1,14 @@
+package com.nestor.springboot.error.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+	@GetMapping("/index")
+	public String index() {
+		// Integer valor = 100/0;
+		return "index";
+	}
+}
