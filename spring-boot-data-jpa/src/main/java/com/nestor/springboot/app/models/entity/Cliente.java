a7c3fff009263b8fae361c4a0cc1e9aaa9081c13
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-04-29 10:16:02 +0200

    Entidad Cliente

---
+++ spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/models/entity/Cliente.java
@@ -0,0 +1,10 @@
+package com.nestor.springboot.app.models.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="clientes")
+public class Cliente {
+
+}
