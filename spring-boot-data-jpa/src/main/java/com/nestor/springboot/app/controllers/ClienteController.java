8345f6ed741a577f25e4edecb71ba581b38dd9fc
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-04-30 12:20:44 +0200

    Agregando editar

--- spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/controllers/ClienteController.java
@@ -5,11 +5,15 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.nestor.springboot.app.dao.IClienteDao;
 import com.nestor.springboot.app.models.entity.Cliente;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ClienteController {
@@ -33,8 +37,28 @@ public class ClienteController {
 		return "form";
 	}
 	
+	@RequestMapping(value="/form/{id}")
+	public String editar(@PathVariable(value="id")Long id,  Map<String, Object> model) {
+		
+		Cliente cliente = null;
+		
+		if(id>0) {
+			cliente = clienteDao.findOne(id);
+		} else {
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar Cliente");
+		return "form";
+	}
+	
 	@RequestMapping(value="/form", method=RequestMethod.POST)
-	public String guardar(Cliente cliente) {
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de Cliente");
+			return "form";
+		}
+				
 		clienteDao.save(cliente);
 		return "redirect:listar";
 	}
