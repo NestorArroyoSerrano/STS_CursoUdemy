685f8c3021e7e3eca004472a20cdd7d176de4d07
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-05-03 10:07:26 +0200

    PageRequest

--- spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/dao/IClienteDao.java
@@ -2,10 +2,10 @@ package com.nestor.springboot.app.dao;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nestor.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
+public interface IClienteDao extends JpaRepository<Cliente, Long> {
 
 }
