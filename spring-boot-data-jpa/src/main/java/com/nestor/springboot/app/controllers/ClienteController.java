46c07b1940f45899ea0394ce1fd63950c665351b
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-05-03 12:53:38 +0200

    PageRender

--- spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/controllers/ClienteController.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.nestor.springboot.app.models.entity.Cliente;
 import com.nestor.springboot.app.service.IClienteService;
+import com.nestor.springboot.app.util.paginator.PageRender;
 
 import jakarta.validation.Valid;
 
@@ -32,12 +33,15 @@ public class ClienteController {
 	@RequestMapping(value="/listar", method=RequestMethod.GET)
 	public String listar(@RequestParam(name="page", defaultValue="0" ) int page, Model model) {
 		
-		Pageable pageRequest = PageRequest.of(page,  4);
+		Pageable pageRequest = PageRequest.of(page,  5);
 		
 		Page<Cliente> clientes = clienteService.findAll(pageRequest);
 		
+		
+		PageRender<Cliente> pageRender = new PageRender<>("/listar", clientes);
 		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clientes);
+		model.addAttribute("page", pageRender);
 		return "listar";
 	}
 	
