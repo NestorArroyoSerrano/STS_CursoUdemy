9bbdc39814dc29f6b149c5b97b08aa262e75d379
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-05-09 13:04:47 +0200

    FacturaController

--- spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/models/entity/Cliente.java
@@ -2,12 +2,17 @@ package com.nestor.springboot.app.models.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -41,7 +46,14 @@ public class Cliente implements Serializable {
 	@DateTimeFormat(pattern="yyyy-MM-dd")
 	private Date createAt;
 
+	@OneToMany(mappedBy="cliente" ,fetch = FetchType.LAZY, cascade=CascadeType.ALL)
+	private List<Factura> facturas;
+	
 	private String foto;
+	
+	public Cliente() {
+		facturas = new ArrayList<Factura>();
+	}
 	
 	public Long getId() {
 		return id;
@@ -93,6 +105,18 @@ public class Cliente implements Serializable {
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
+	}
+
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+	
+	public void addFactura(Factura factura) {
+		facturas.add(factura);
 	}
 
 	private static final long serialVersionUID = 1L;
