03b5bd6830c59ad6813b64813021b2a0e29cab5c
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-05-09 13:51:19 +0200

    /form

--- spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/controllers/FacturaController.java
+++ spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/controllers/FacturaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.nestor.springboot.app.models.entity.Cliente;
@@ -15,6 +16,7 @@ import com.nestor.springboot.app.models.service.IClienteService;
 
 @Controller
 @RequestMapping("/factura")
+@SessionAttributes("factura")
 public class FacturaController {
 	
 	@Autowired
@@ -31,6 +33,10 @@ public class FacturaController {
 			return "redirect:/listar";
 		}
 		Factura factura = new Factura();
+		factura.setCliente(cliente);
+		
+		model.put("factura", factura);
+		model.put("titulo", "Crear Factura");
 		
 		return "factura/form";
 	}
