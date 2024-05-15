fd356986ff2cd10d22d756a30cc4413960d3cd37
NestorArroyoSerrano <122863244+NestorArroyoSerrano@users.noreply.github.com>
2024-05-15 09:47:57 +0200

    App para crear facturas terminada

--- spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/models/service/ClienteServiceImpl.java
+++ spring-boot-data-jpa/src/main/java/com/nestor/springboot/app/models/service/ClienteServiceImpl.java
@@ -88,4 +88,11 @@ public class ClienteServiceImpl implements IClienteService {
 		
 		return facturaDao.findById(id).orElse(null);
 	}
+
+	@Override
+	@Transactional
+	public void deleteFactura(Long id) {
+		facturaDao.deleteById(id);
+		
+	}
 }
